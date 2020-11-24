a7bd771a37db5235e2322b8e5e1ae1d5ded78c1e
fynjy <antony.ets@yandex.ru>
2020-11-24 11:02:59 +0300

    fixes

--- src/KafkaTransport.php
+++ src/KafkaTransport.php
@@ -5,27 +5,78 @@ namespace lliytnik\eventbus\transports;
 
 
 use lliytnik\eventbus\events\Event;
+use lliytnik\eventbus\transports\Transport;
+use lliytnik\kafka\Consumer;
+use lliytnik\kafka\Producer;
+use lliytnik\kafka\Sender;
 
 class KafkaTransport extends Transport
 {
+    public $kafka;
+    public $config;
+    public $producer;
+    public $consumer;
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
 
     function send(Event $event)
     {
-        // TODO: Implement send() method.
+        if(!$this->producer){
+            $this->initProducer();
+        }
+        $this->producer->send($this->encodeEvent($event));
     }
 
     function consume()
     {
-        // TODO: Implement consume() method.
+        if(!$this->consumer){
+            $this->initConsumer();
+        }
+        $this->consumer->consume();
     }
 
     function encodeEvent(Event $event)
     {
-        // TODO: Implement encodeEvent() method.
+        return json_encode($event);
     }
 
-    function decodeEvent(Event $event)
+    function decodeEvent(string $event)
     {
-        // TODO: Implement decodeEvent() method.
+        return json_decode($event);
+    }
+
+    protected function initProducer(){
+        $this->producer = new Producer($this->config);
+    }
+
+    protected function initConsumer(){
+        $this->consumer = new Consumer($this->config);
+    }
+
+    public function setMessageCallBack($messageCallBack){
+        $this->consumer->messageCallBack = $messageCallBack;
+    }
+
+    public function setErrorCallBack($errorCallBack){
+        $this->consumer->errorCallBack = $errorCallBack;
+    }
+
+    function getConsumer()
+    {
+        if(!$this->consumer){
+            $this->initConsumer();
+        }
+        return $this->consumer;
+    }
+
+    function getProducer()
+    {
+        if(!$this->getProducer()){
+            $this->initProducer();
+        }
+        return $this->producer;
     }
 }
